c69af759339fbcdd9faf28000df60ebf1a98cd0e
AnulusDev <khalidalhazan@gmail.com>
2026-04-04 00:13:29 +0100

    feat: implement profile sync signal with role transition handling

--- account/signals/handlers.py
+++ account/signals/handlers.py
@@ -1,13 +1,42 @@
 from django.conf import settings
+from django.db import transaction
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 from account.models import LecturerProfile, StudentProfile
 
+@receiver(pre_save, sender=settings.AUTH_USER_MODEL)
+def capture_old_role(sender, instance, **kwargs):
+    if instance.pk:
+        try:
+            instance._old_role = sender.objects.get(pk=instance.pk).role
+        except sender.DoesNotExist:
+            instance._old_role = None
+    else:
+        instance._old_role = None
+
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
-def create_profile_for_new_user(sender, created, instance, **kwargs):
+def sync_profile_on_save(sender, created, instance, **kwargs):
     if created:
         if instance.is_student:
             StudentProfile.objects.get_or_create(user=instance)
-        if instance.is_lecturer:
+        elif instance.is_lecturer:
             LecturerProfile.objects.get_or_create(user=instance)
+        return
+
+        old_role = getattr(instance, '_old_role', None)
+        if old_role == instance.role:
+            return
+
+    handle_profile_transition(instance, old_role)
+
+
+def handle_profile_transition(user, old_role):
+    with transaction.atomic():
+        if user.is_student:
+            LecturerProfile.objects.filter(user=user).update(is_active=False)
+            StudentProfile.objects.get_or_create(user=user)
+
+        elif user.is_lecturer:
+            StudentProfile.objects.filter(user=user).update(is_active=False)
+            LecturerProfile.objects.get_or_create(user=user)
